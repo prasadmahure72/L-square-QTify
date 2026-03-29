5b5523ec87b5575eb7d1c0dd9396e81a6052e754
Prasad M <prasad@promaxevents.in>
2026-03-29 13:51:19 +0530

    Test Case

--- qtify/src/App.test.js
+++ qtify/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import axios from "axios";
+import App from "./App";
 
-test('renders learn react link', () => {
+jest.mock("axios");
+
+const mockData = require("../response.json");
+
+test("should match the count of top, and new album cards with their respective API responses", async () => {
+  axios.get.mockImplementation((url) => {
+    if (url.includes("/albums/top")) return Promise.resolve({ data: mockData });
+    if (url.includes("/albums/new")) return Promise.resolve({ data: mockData });
+    return Promise.resolve({ data: [] });
+  });
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const cards = await screen.findAllByTestId("card");
+  expect(cards.length).toBe(mockData.length * 2);
 });
